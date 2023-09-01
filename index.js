e7746e7a8eb8ebd80cb17777f83983141ddb6c8b
Sanyam Srivastava <105932014+Sanyamsri@users.noreply.github.com>
2023-09-01 15:12:22 +0530

    corrected

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-let prevButton=document.getElementById('prev')
-console.log(prevButton)
-let nextButton=document.getElementById('next')
-prevButton.addEventListener("click", ()=>{
-    console.log("pop")
-    let container=document.getElementById('container')
-    container.scrollLeft+=100;
+let prevButton = document.getElementById("prev");
+let nextButton = document.getElementById("next");
+let container = document.getElementsByClassName("items");
+let scrollIndex = 1;
+
+prevButton.addEventListener("click", () => {
+  nextButton.style.display = "revert";
+  if (scrollIndex === 0) {
+    prevButton.style.display = "none";
+  }
+  scrollIndex = Math.max(scrollIndex - 1, 0);
+  Array.prototype.map.call(
+    container,
+    (card) => (card.style.translate = `-${scrollIndex * card.offsetWidth}px`)
+  );
+});
+
+nextButton.addEventListener("click", () => {
+  prevButton.style.display = "revert";
+  if (scrollIndex === container.length) {
+    nextButton.style.display = "none";
+  }
+  scrollIndex = Math.min(container.length, scrollIndex + 1);
+  Array.prototype.map.call(
+    container,
+    (card) => (card.style.translate = `-${scrollIndex * card.offsetWidth}px`)
+  );
 });
